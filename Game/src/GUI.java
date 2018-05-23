ab76737e4796430242c80f3b504cd60ec804af2d
EherSenaw <onow7353@gmail.com>
2018-05-23 15:42:13 +0900

    Practice: DONUT!

--- Game/src/GUI.java
+++ Game/src/GUI.java
@@ -1,3 +1,6 @@
+/*
+    Studying with: "http://zetcode.com/tutorials/javagamestutorial/basics/"
+*/
 import javax.swing.*;
 
 public class GUI extends JFrame {
@@ -11,7 +14,7 @@ public class GUI extends JFrame {
 
         setSize(800, 600);
 
-        setTitle("Application");
+        setTitle("Donut");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
     }
