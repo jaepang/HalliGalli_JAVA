7964fdaf06d9c5001c165e790280fbd71d3df161
shinjawkwang <shinjawkwang@naver.com>
2018-05-30 13:54:39 +0900

    Bell currently works, Card does not.

--- Game/src/MainFrame.java
+++ Game/src/MainFrame.java
@@ -11,21 +11,31 @@ public class MainFrame extends JFrame{
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setTitle("Halli-Galli");
 
+        /*Add panel to Frame*/
+        this.add(panel);
+
+        /* Set Boundary */
+        panel.setBounds(0, 0, 800, 600);
+
 		/*Create "Bell" bell Instance*/
 		Bell bell = new Bell();
-        panel.add(bell, new Integer(0), 0);
+        bell.setOpaque(true);
+        bell.setBounds(0, 0, 800, 600);
 
 		/*Card Class Test*/
-        Card test = new Card(); bell.add(test);
-        panel.add(test, new Integer(1), 0);
+        //Card test = new Card(); bell.add(test);
+        //test.setOpaque(true);
+        //panel.setBounds(0, 0, 100, 100);
 
         /*Card Class Test2: Testing image*/
         Card test2 = new Card("Banana",1);
-        panel.add(test2, new Integer(2), 0);
+        test2.setOpaque(true);
+        panel.setBounds(0, 0, 100, 100);
 
-        //this.pack();
-        /*Add bell to Frame*/
-        this.add(panel);
+        panel.add(bell, new Integer(0), 0);
+        //panel.add(test, new Integer(1), 0);
+        panel.add(test2, new Integer(2), 0);
+        this.pack();
         this.setVisible(true);
 	} 
 	
