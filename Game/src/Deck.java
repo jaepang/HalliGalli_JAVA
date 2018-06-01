53b611a1bf5e501b66d93c0e33ee6cf2ec2cdee3
shinjawkwang <shinjawkwang@naver.com>
2018-06-01 16:50:50 +0900

    made getFruit, getCnt method because of private; deck topcard displaying test succeed;

--- Game/src/Deck.java
+++ Game/src/Deck.java
@@ -1,14 +1,52 @@
 import java.util.ArrayList;
+import java.util.*;
 
 public class Deck extends Card{
-    private ArrayList<Card> cards;
+    private ArrayList<Card> cards = new ArrayList<Card>();
     private Card topCard;
     Deck(){
         cards.clear();
         topCard = null;
     }
     public void addCard(Card arg1){
-        cards.add(arg1);
+        this.cards.add(arg1);
         topCard = arg1;
+    }
+    public Card getTopCard(){
+        return this.topCard;
+    }
+    /* make deck and shuffle */
+    public void createDeck(){
+        for(int i=0; i<4; i++){
+            switch (i){
+                case 0:
+                    for(int j=0; j<14; j++){ //012 345 678 91011 1213
+                        Card card = new Card("Banana", j/3);
+                        addCard(card);
+                    }
+                    break;
+                case 1:
+                    for(int j=0; j<14; j++){ //012 345 678 91011 1213
+                        Card card = new Card("Grapefruit", j/3);
+                        addCard(card);
+                    }
+                    break;
+                case 2:
+                    for(int j=0; j<14; j++){ //012 345 678 91011 1213
+                        Card card = new Card("Kiwi", j/3);
+                        addCard(card);
+                    }
+                    break;
+                case 3:
+                    for(int j=0; j<14; j++){ //012 345 678 91011 1213
+                        Card card = new Card("Strawberry", j/3);
+                        addCard(card);
+                    }
+                    break;
+            }
+        }
+        long seed = System.nanoTime();
+        Collections.shuffle(this.cards, new Random(seed));
+        this.topCard = this.cards.get(0);
     }
 }
--- Game/src/MainFrame.java
+++ Game/src/MainFrame.java
@@ -3,6 +3,16 @@ import java.awt.*;
 
 public class MainFrame extends JFrame{
     private JLayeredPane panel = new JLayeredPane();
+    /* Check whether it is time to ring the bell */
+    /* When player gets more than 2, we may need ArrayList <Deck> */
+    public boolean Ringdingdong(Deck one, Deck two){
+        if(one.getTopCard().getFruit() == two.getTopCard().getFruit()){
+            if(one.getTopCard().getCnt()+two.getTopCard().getCnt() == 5){
+                return true;
+            }
+        }
+        return false;
+    }
 	public MainFrame() {
 		int width, height;
 		width = 800;
@@ -23,16 +33,20 @@ public class MainFrame extends JFrame{
         bell.setOpaque(true);
         bell.setBounds(0, 0, 800, 600);
 
-		/*Card Class Test*/
-        //Card test = new Card();
-        //panel.setBounds(0, 0, 100, 100);
+		/*Deck Class Test*/
+        Deck testDeck = new Deck();
+        testDeck.createDeck();
+        Card test = testDeck.getTopCard();
+        test.setBounds(0, 0, 100, 100);
 
         /*Card Class Test2: Testing image*/
-        Card test2 = new Card("Banana",1);
+        Deck testDeck2 = new Deck();
+        testDeck2.createDeck();
+        Card test2 = testDeck2.getTopCard();
         test2.setBounds(100, 100, 100, 100);
 
         panel.add(bell, new Integer(0));
-        //panel.add(test, new Integer(1), 0);
+        panel.add(test, new Integer(1), 0);
         panel.add(test2, new Integer(1));
         this.pack();
         this.setVisible(true);
