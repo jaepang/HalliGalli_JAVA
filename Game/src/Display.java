dae3f92dc7b55370ae193d63eaeeefe0b9cb397f
EherSenaw <onow7353@gmail.com>
2018-06-12 13:07:36 +0900

    "Display" class needed. Check TODO in this class.

---
+++ Game/src/Display.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class Display extends JFrame {
+
+    /* TODO: This class should do following things:
+            - Display game GUIs for each players, differently!
+            - This might be converting "MainFrame" class
+            - Be aware of that this is module to utilize displaying for server-client association easier.
+    */
+
+
+}
